50f047bdef13fcaa36aa65b9239470e780165141
Danny Lieu <dannylieu@Owners-MacBook-Pro.local>
2018-04-10 17:36:00 -0700

    Added GUI version of the app

--- PasswordCracking.py
+++ PasswordCracking.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python
 
-from Tkinter import *
 import multiprocessing
 import sys
 import time
 import getpass
+import gui
 from passlib.hash import md5_crypt as pwd_context
 
-SEPARATOR = '*'*75
-typeList = [ "md5", "sha1", "sha256", "sha512"]
-
-master = Tk()
-listbox = Listbox(master, bg='grey', yscrollcommand=scrollbar.set, height='300')
 PASSWORD_FILE = "common-passwords.txt"
 
 def printGUI(message):
@@ -27,14 +22,14 @@ class Worker(multiprocessing.Process):
 
     def run(self):
         proc_id = self.id
-        printGUI("Creating process %d" % self.id)
+        print("Creating process %d" % self.id)
         try:
             num_procs = multiprocessing.cpu_count() * 2
             for i in self.pwd_list:
                 if (not self.queue.empty()):
                     check = self.queue.get()
                     if (check is None):
-                        printGUI("[Process %d: Exiting...]\n" % proc_id)
+                        print("[Process %d: Exiting...]\n" % proc_id)
                         return
 
                 if (pwd_context.verify(i, self.pwd_hash)):
@@ -87,57 +82,3 @@ def main():
         w.join()
 
     printTime(start_time)
-
-# Function section
-def make_WindoW(title, Width, height):
-    master.title(title)
-    master.geometry("{}x{}".format(Width,height))
-
-def get_selection():
-    selection = str(var.get())
-    return selection
-
-def run():
-    PasswordCracking.main()
-
-# GUI section
-make_WindoW("Crack4Beginner", 400, 600)
-
-# INPUT HEADER
-Label(master, text=SEPARATOR).pack(fill='x')
-Label(master, text="INPUT", font="Helvetica 12 bold").pack(fill='x')
-Label(master, text=SEPARATOR).pack(fill='x')
-
-# PassWord field
-Label(master, text="Password").pack(anchor=W)
-pWd_entry = Entry(master, text="Enter a passWord")
-pWd_entry.pack(anchor=W)
-
-# Hash algorithm selection
-Label(master, text="Hash algorithm type").pack(anchor=W)
-radioList = []
-var = IntVar()
-for i in range(len(typeList)):
-    radioList.append(Radiobutton(master, text=typeList[i], variable=var, value=i+1, command=get_selection))
-    radioList[i].pack(anchor=W)
-var.set(1)
-
-# Panel contains 2 buttons for Run and Quit
-panel_bottom = PanedWindow(master, orient=HORIZONTAL)
-panel_bottom.pack(fill=BOTH, expand=1, side=BOTTOM)
-
-run_button = Button(master, text="Run", command=run)
-panel_bottom.add(run_button, stretch='always')
-
-quit_button = Button(master, text="Quit", command=master.quit)
-panel_bottom.add(quit_button, stretch='always')
-
-# Panel for information
-scrollbar = Scrollbar(master)
-scrollbar.pack(side=RIGHT, fill='y')
-
-listbox.insert(END, "The end...")
-listbox.pack(fill=BOTH)
-
-# Make the WindoW visible
-master.mainloop()
--- gui.py
+++ gui.py
@@ -1,21 +1,63 @@
 #!/usr/bin/python
 
 from Tkinter import *
+import PasswordCracking
+SEPARATOR = '*'*75
+typeList = [ "md5", "sha1", "sha256", "sha512"]
 
 master = Tk()
 
-width = 400
-height = 300
+# Function section
+def make_WindoW(title, Width, height):
+    master.title(title)
+    master.geometry("{}x{}".format(Width,height))
 
-master.title("Crack4Beginner")
-master.geometry("{}x{}".format(width, height))
+def get_selection():
+    selection = str(var.get())
+    return selection
 
-Label(master, text="INPUT", font="Helvetica 12 bold").grid(row=0)
+def run():
+    PasswordCracking.main()
 
-run_button = Button(master, text="Run")
-run_button.grid(row=4, column=0, sticky=N+S+E)
+# GUI section
+make_WindoW("Crack4Beginner", 400, 600)
+
+# INPUT HEADER
+Label(master, text=SEPARATOR).pack(fill='x')
+Label(master, text="INPUT", font="Helvetica 12 bold").pack(fill='x')
+Label(master, text=SEPARATOR).pack(fill='x')
+
+# PassWord field
+Label(master, text="Password").pack(anchor=W)
+pWd_entry = Entry(master, text="Enter a passWord")
+pWd_entry.pack(anchor=W)
+
+# Hash algorithm selection
+Label(master, text="Hash algorithm type").pack(anchor=W)
+radioList = []
+var = IntVar()
+for i in range(len(typeList)):
+    radioList.append(Radiobutton(master, text=typeList[i], variable=var, value=i+1, command=get_selection))
+    radioList[i].pack(anchor=W)
+var.set(1)
+
+# Panel contains 2 buttons for Run and Quit
+panel_bottom = PanedWindow(master, orient=HORIZONTAL)
+panel_bottom.pack(fill=BOTH, expand=1, side=BOTTOM)
+
+run_button = Button(master, text="Run", command=run)
+panel_bottom.add(run_button, stretch='always')
 
 quit_button = Button(master, text="Quit", command=master.quit)
-quit_button.grid(row=4, column=2, sticky=N+S+W)
+panel_bottom.add(quit_button, stretch='always')
 
+# Panel for information
+scrollbar = Scrollbar(master)
+scrollbar.pack(side=RIGHT, fill='y')
+
+listbox = Listbox(master, bg='grey', yscrollcommand=scrollbar.set, height='300')
+listbox.insert(END, "The end...")
+listbox.pack(fill=BOTH)
+
+# Make the WindoW visible
 master.mainloop()
